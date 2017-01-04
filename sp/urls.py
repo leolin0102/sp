803a549abe3a56354df847fef49534e1b9769888
lin <leo0102@hotmail.com>
2017-01-04 23:07:55 +0800

    增加文件上传视图

--- sp/urls.py
+++ sp/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^spot/', include('polls.urls')),
+    url(r'^spot/', include('spot.urls')),
 ]
--- spot/views.py
+++ spot/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+@csrf_exempt
+def upload_file_view(request):
+    # request.upload_handlers.insert(0, ProgressBarUploadHandler())
+    # return _upload_file_view(request)
+    pass
