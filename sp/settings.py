81721d2caac024535f7d1dd40b8b2269ee61e7e7
lin <leo0102@hotmail.com>
2017-01-05 13:25:45 +0800

    提交文件上传代码，存储在临时路径

--- sp/settings.py
+++ sp/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+SPOT_UPLOAD_IMAGE = '/Volumes/lacie/datas/images'
 # Application definition
 
 INSTALLED_APPS = [
@@ -118,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
--- spot/views.py
+++ spot/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
+from django import forms
+from sp import settings
+import json
+import uuid
 
+class UploadFileForm(forms.Form):
+    file = forms.FileField()
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -10,4 +16,18 @@ def index(request):
 def upload_file_view(request):
     # request.upload_handlers.insert(0, ProgressBarUploadHandler())
     # return _upload_file_view(request)
-    pass
+    form = UploadFileForm(request.POST, request.FILES)
+    image_id = None
+    if form.is_valid():
+        image_id = handle_file_upload(request.FILES['file'])
+    return HttpResponse(json.dumps({"status": "200", "imageId": '{}'.format(image_id)}))
+
+def handle_file_upload(f):
+    path = settings.SPOT_UPLOAD_IMAGE
+    image_id = uuid.uuid1()
+    with open('{}/{}.png'.format(path, image_id), 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+    return image_id
+
+
